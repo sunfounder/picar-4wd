11ad56ddae4f611ccc33213406b6aa03345f9c43
dino <845864704@qq.com>
2019-09-29 10:55:42 +0800

    setup.py add reboot

--- setup.py
+++ setup.py
@@ -158,7 +158,8 @@ def install():
         cmd='run_command("sudo cp ./bin/picar-4wd-web-example /etc/init.d/picar-4wd-web-example")')
     do(msg="add excutable mode for picar-4wd-web-example",
         cmd='run_command("sudo chmod +x /etc/init.d/picar-4wd-web-example")')
-
+    do(msg="System will reboot now",
+    cmd='run_command("sudo reboot")')
 install()
 
 setup(
