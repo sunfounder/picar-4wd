55a9f98e4c58d04110628883604f0e8598d3ca47
Anthony Doan <=>
2023-09-06 21:05:10 -0500

    Add initial logic for containing picar's position (x, y, angle)

--- cs437/lab_1b_more_advanced_mapping.py
+++ cs437/lab_1b_more_advanced_mapping.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import picar_4wd as fc
 
 # Initialize the map
@@ -6,13 +7,34 @@ map_width = 100
 map_height = 100
 picar_map = np.zeros((map_width, map_height), dtype=int)
 
-def print_map(world_map):
-    # This function prints the map out for visualization
+# Initialize picar's positioning as well as its speed for movement/turning
+picar_position = {
+    'x': 0,
+    'y': 0,
+    'angle': 0   
+}
+
+velocity = {
+    'linear': 0.1,    
+    'turning': 5     
+}
+
+servo_step_angle = 5
+
+def print_map(world_map, car_position):
+    # This function prints the map out for visualization and car's positioning
     for row in world_map:
         print("".join(["#" if cell == 1 else " " for cell in row]))
+    print(f"Car (X, Y, Angle): ({car_position['x']}, {car_position['y']}, {car_position['angle']})")
+
+def update_car_position(current_position, velocity):
+    # Update the current position of the car based on the provided velocity
+    current_position['x'] += velocity['linear'] * np.cos(np.radians(current_position['angle']))
+    current_position['y'] += velocity['linear'] * np.sin(np.radians(current_position['angle']))
+    current_position['angle'] += velocity['turning']
 
 def update_map(picar_map, car_position, threshold):
-    for angle in range(0, 181, 5):  # Rotate the servo between 0 and 180 degrees at 5 degree increments
+    for angle in range(0, 181, servo_step_angle):  # Rotate the servo between 0 and 180 degrees at 5 degree increments
         # Get the distance reading from the ultrasonic sensor
         distance = fc.get_distance_at(angle)
         
@@ -27,16 +49,21 @@ def update_map(picar_map, car_position, threshold):
             if distance <= threshold:
                 picar_map[y, x] = 1
     
+        # Update car's positioning based on how far it drove
+        update_car_position(picar_position, velocity)
+        
         # Prints out the map for users to see waht the sensor sees
         print_map(picar_map)
 
 # SLAM with ultrasonic sensor
 def slam():
-    # Initialize picar's position
-    car_position = (0, 0)
     threshold = 100  # Set threshold (can adjust as needed)
     while True:
-        update_map(picar_map, car_position, threshold)
+        update_map(picar_map, picar_position, threshold)
+        fc.forward()
+        time.sleep(1)
+        fc.stop()
+        time.sleep(1)
 
 if __name__ == "__main__":
     try: 
