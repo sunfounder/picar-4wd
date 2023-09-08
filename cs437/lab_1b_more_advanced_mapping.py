2127799406c547c737e7a80a6fbcd4234d76d1eb
Anthony Doan <=>
2023-09-07 19:07:46 -0500

    Testing localization stuff

--- cs437/lab_1b_more_advanced_mapping.py
+++ cs437/lab_1b_more_advanced_mapping.py
@@ -74,12 +74,16 @@ def update_map(car_position, threshold):
         us_step = -servo_step_angle  # Reverse direction
          # Clear the map at the beginning of each scan
         picar_map = np.zeros((map_width, map_height), dtype=int)
-        print("END on RIGHT?")
     elif current_angle <= -180:
         current_angle = -180
         us_step = servo_step_angle  # Reverse direction
          # Clear the map at the beginning of each scan
         picar_map = np.zeros((map_width, map_height), dtype=int)
+        fc.forward(velocity['linear'])
+        time.sleep(1)
+        fc.stop()
+        time.sleep(1)
+        update_car_position(picar_position, velocity)
 
     # Clear the console and print the current state of the map and robot's pose
     clear_console()
