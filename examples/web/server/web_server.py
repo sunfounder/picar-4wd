46adbb6e7db417683eb3b3a11e733646771015d3
Lovmong <1967471283@qq.com>
2024-03-06 09:21:19 +0800

    optimize

--- examples/web/server/web_server.py
+++ examples/web/server/web_server.py
@@ -1,163 +1,288 @@
-import picar_4wd as fc
-from picar_4wd.utils import pi_read
-from remote_control import Remote_control
-from picar_4wd import getIP
+from picar_4wd.utils import pi_read, getIP, soft_reset
+from picar_4wd import Picar4WD
 
 import asyncio
 import websockets
+import threading
 import json
 import time
 
-fc.start_speed_thread()
+# --- init picar_4wd ---
+mycar = Picar4WD()
+mycar.start_speed_thread()
+
+# --- variables ---
+PORT = 8765
+
+OBSTACLE_AVOID_REFERENCE = 35
+line_reference = 400
+cliff_reference = 110
+
 speed_count = 0
 gs_list = []
-
-
+speed_data = []
+scan_list = False
+pi_info = {}
+power = 0
+client_num = 0
 recv_dict = {
-    'RC':'forward',
+    'RC': 'off',
     'GS': "off",
-    'RD':'off',
-    'OA':'off',
-    'OF':'off',
-    'TL':['off',400],
-    'CD':['off',110],
-    'PW':1,
-    'SR':0,
-    'ST':'off',
-    'US':['on',0],
-    'MS':['off',0,0]
+    'RD': 'off',
+    'OA': 'off',
+    'OF': 'off',
+    'TL': ['off', line_reference],
+    'CD': ['off', cliff_reference],
+    'PW': 0,
+    'SR': 0,
+    'ST': 'off',
+    'US': ['off', 0],
+    'MS': ['off', 0, 0]
 }
 
 send_dict = {
-    'GS': [0,0,0],
-    'US':[0,0],
-    'MS':[0,0],
-    'ST':{'a':1}
-} 
-  
+    'GS': [0, 0, 0],
+    'US': [0, 0],
+    'MS': [0, 0],
+    'ST': {
+        'a': 1
+    },
+}
+
+adc_thread_lock = threading.Lock()
 
 
-async def recv_server_func(websocket):
-    global recv_dict,send_dict
-    while 1:
-        tmp = await websocket.recv()
-        tmp = json.loads(tmp)
-        for key in tmp:
-            recv_dict[key] = tmp[key]
-        recv_dict['PW'] = int(recv_dict['PW'])
-        Remote_control(recv_dict['RC'],recv_dict['PW'])
-        # print(recv_dict)
-        if  recv_dict['MS'][0] =='on':
-            fc.set_motor_power(int(recv_dict['MS'][1]), int(recv_dict['MS'][2]))
-        if  recv_dict['SR'] =='on':
-            fc.soft_reset()
+# ------------------ track line  ------------------
+def track_line(ref, power):
+    if mycar.get_line_status(ref, gs_list) == 0:
+        mycar.forward(power)
+    elif mycar.get_line_status(ref, gs_list) == -1:
+        mycar.turn_left(power)
+    elif mycar.get_line_status(ref, gs_list) == 1:
+        mycar.turn_right(power)
 
 
+# ------------------ obstacle_avoid ------------------
+def obstacle_avoid(power):
+    if not scan_list:
+        return
+
+    tmp = scan_list[3:7]
+    if tmp != [2, 2, 2, 2]:
+        mycar.turn_right(power)
+    else:
+        mycar.forward(power)
 
 
-async def send_server_func(websocket): 
-    global send_dict, recv_dict, gs_list 
-    while 1:
-        send_dict ={}
-        send_dict['MS'] = [round(fc.speed_val()/2.0),time.time()] 
-        
+# ------------------ obstacle_follow ------------------
+def obstacle_follow(power):
+    if not scan_list:
+        return
 
-        if recv_dict['ST'] == 'on': 
-            send_dict['ST'] = pi_read() 
-
-        if  recv_dict['US'][0] =='on':
-            send_dict['US'] = [int(recv_dict['US'][1]),fc.get_distance_at(int(recv_dict['US'][1]))]
+    scan_list_temp = [str(i) for i in scan_list]
+    scan_list_temp = "".join(scan_list_temp)
+    paths = scan_list_temp.split("2")
+    length_list = []
+    for path in paths:
+        length_list.append(len(path))
+    if max(length_list) == 0:
+        mycar.stop()
+    else:
+        i = length_list.index(max(length_list))
+        pos = scan_list_temp.index(paths[i])
+        pos += (len(paths[i]) - 1) / 2
+        delta = len(scan_list_temp) / 3
+        if pos < delta:
+            mycar.turn_left(power)
+        elif pos > 2 * delta:
+            mycar.turn_right(power)
         else:
-            send_dict['US'] = fc.angle_distance
-        
-        if  recv_dict['GS'] =='on': 
-            send_dict['GS'] = gs_list
-        await websocket.send(json.dumps(send_dict))
-        await asyncio.sleep(0.01)
-        
-async def main_func():
-    global recv_dict,send_dict,gs_list
-    while 1:
-        gs_list = fc.get_grayscale_list()
-        
-        if recv_dict['CD'][0] == 'on':
-            if fc.is_on_edge(recv_dict['CD'][1],gs_list):
-                fc.backward(20)
-                time.sleep(0.5)
-                fc.stop()
+            if scan_list_temp[int(len(scan_list_temp) / 2 - 1)] == "0":
+                mycar.backward(power)
+            else:
+                mycar.forward(power)
 
-        if recv_dict['TL'][0] =='on':
-            if fc.get_line_status(recv_dict['TL'][1],gs_list) == 0:
-                fc.forward(recv_dict['PW'])      
-            elif fc.get_line_status(recv_dict['TL'][1],gs_list) == -1:
-                fc.turn_left(recv_dict['PW'])
-            elif fc.get_line_status(recv_dict['TL'][1],gs_list) == 1:
-                fc.turn_right(recv_dict['PW']) 
 
-        if recv_dict['OA'] == 'on':
-            scan_list = fc.scan_step(35)
-            if scan_list:
-                tmp = scan_list[3:7]
-                if tmp != [2,2,2,2]:
-                    fc.turn_right(recv_dict['PW'])
-                else:
-                    fc.forward(recv_dict['PW'])
+# ------------------ remote_control ------------------
+def remote_control(car, control_flag, speed=50):
+    speed = int(speed)
+    if control_flag == 'forward':
+        car.forward(speed)
+    elif control_flag == 'backward':
+        car.backward(speed)
+    elif control_flag == 'turn_left':
+        car.turn_left(speed)
+    elif control_flag == 'turn_right':
+        car.turn_right(speed)
+    else:
+        car.stop()
 
-        elif recv_dict['OF'] == 'on':
-            scan_list = fc.scan_step(23)
-            
-            if scan_list != False:
-                scan_list = [str(i) for i in scan_list]
-                scan_list = "".join(scan_list)
-                paths = scan_list.split("2")
-                length_list = []
-                for path in paths:
-                    length_list.append(len(path))
-                if max(length_list) == 0:
-                    fc.stop() 
-                else:
-                    i = length_list.index(max(length_list))
-                    pos = scan_list.index(paths[i])
-                    pos += (len(paths[i]) - 1) / 2
-                    delta = len(scan_list) / 3
-                    if pos < delta:
-                        fc.turn_left(recv_dict['PW'])
-                    elif pos > 2 * delta:
-                        fc.turn_right(recv_dict['PW'])
-                    else:
-                        if scan_list[int(len(scan_list)/2-1)] == "0":
-                            fc.backward(recv_dict['PW'])
-                        else:
-                            fc.forward(recv_dict['PW'])
-    
-        elif  recv_dict['RD'] == 'on':
-            fc.scan_step(35)
-      
-        await asyncio.sleep(0.01)
-        
-async def main_logic_1(websocket,path):
-    while 1:
-        await recv_server_func(websocket)
 
-async def main_logic_2(websocket,path):
-    while 1:
-        await send_server_func(websocket)
+# ------------------ websockets serve ------------------
+async def websockets_handler(websocket):
+    global recv_dict, send_dict, client_num
+    # ---- clien connected ----
+    _client_ip = websocket.remote_address[0]
+    print(f'client {_client_ip} connected')
+    client_num += 1
+    print(f'client {client_num}')
+    # ---- event loop ----
+    while True:
+        try:
+            # -- recv --
+            try:
+                tmp = await asyncio.wait_for(websocket.recv(), timeout=0.001)
+                print("websocket.recv() temp: %s" % tmp)
 
-try:
-    for _ in range(10):
-        ip = getIP()
-        if ip:
-            print("IP Address: "+ ip)
-            # start_http_server()
+                tmp = json.loads(tmp)
+                for key in tmp:
+                    recv_dict[key] = tmp[key]
+            except asyncio.TimeoutError as e:
+                pass
+
+            # -- send --
+            try:
+                send_tmp = send_dict.copy()
+                await websocket.send(json.dumps(send_tmp))
+            except Exception as e:
+                print('send Exception: %s' % e)
+
+            # --- delay ---
+            await asyncio.sleep(0.01)
+
+        except websockets.exceptions.ConnectionClosed as connection_code:
+            print(f'client {_client_ip} disconneted')
+            print(f'{connection_code}')
             break
-        time.sleep(1)
-    start_server_1 = websockets.serve(main_logic_1, ip, 8765)
-    start_server_2 = websockets.serve(main_logic_2, ip, 8766)
-    print('Start!')
-    tasks = [main_func(),start_server_1,start_server_2]
-    asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
-    asyncio.get_event_loop().run_forever()
- 
-finally:
-    print("Finished")
-    fc.stop()
+    client_num -= 1
+
+
+async def websocket_serve_set():
+    async with websockets.serve(websockets_handler, "0.0.0.0", PORT):
+        await asyncio.Future()  # run forever
+
+
+def websocket_run():
+    asyncio.run(websocket_serve_set())
+
+
+def start_serve():
+    serve_thread = threading.Thread(target=websocket_run)
+    serve_thread.daemon = True
+    serve_thread.start()
+
+
+# ------------------ system_info_read_thread ------------------
+def system_info_read_handler():
+    global send_dict, pi_info
+
+    while True:
+        # pi system info
+        if recv_dict['ST'] == 'on':
+            with adc_thread_lock:
+                send_dict['ST'] = pi_read()
+        else:
+            # print('system_info_read pass')
+            pass
+        time.sleep(0.5)
+
+
+def start_system_info_thread():
+    _thread = threading.Thread(target=system_info_read_handler)
+    _thread.daemon = True
+    _thread.start()
+
+
+# ------------------ main ------------------
+def main():
+    global send_dict, recv_dict, gs_list, scan_list
+    global power, cliff_reference, line_reference
+
+    try:
+        # --- get ip ---
+        for _ in range(10):
+            ip = getIP()
+            if ip:
+                # print("IP Address: "+ ip)
+                break
+            time.sleep(1)
+        # --- start ---
+        print(f'web server start at: http://{ip}')
+        start_serve()
+        start_system_info_thread()
+
+        # --- main loop ---
+        while True:
+            # ------------------ get data ------------------
+            # speed
+            send_dict['MS'] = [round(mycar.speed_val() / 2.0), time.time()]
+
+            # ultrasonic
+            if recv_dict['RD'] == 'on':
+                scan_list = mycar.scan_step(OBSTACLE_AVOID_REFERENCE)
+            elif recv_dict['US'][0] == 'on':
+                mycar.get_distance_at(int(recv_dict['US'][1]))
+
+            send_dict['US'] = mycar.angle_distance
+
+            # grayscale
+            if recv_dict['GS'] == 'on' or recv_dict['CD'][0] == 'on':
+                with adc_thread_lock:
+                    gs_list = mycar.get_grayscale_list()
+                    send_dict['GS'] = gs_list
+
+            # ------------------ control ------------------
+            # --- power set ---
+            power = int(recv_dict['PW'])
+
+            # --- reset ---
+            if recv_dict['SR'] == 'on':
+                soft_reset()
+
+            # --- single wheel control (setting) ---
+            if recv_dict['MS'][0] == 'on':
+                mycar.set_motor_power(int(recv_dict['MS'][1]),
+                                      int(recv_dict['MS'][2]))
+
+            # --------------- manual ---------------
+            # --- cliff detect ---
+            if recv_dict['CD'][0] == 'on':
+                cliff_reference = recv_dict['CD'][1]
+                if mycar.is_on_edge(cliff_reference, gs_list):
+                    if recv_dict['RC'] == 'backward':
+                        remote_control(mycar, 'backward', power)
+                    else:
+                        mycar.stop()
+                    continue  # Do not execute the content below
+
+            _is_auto = False
+            # --- track line ---
+            if recv_dict['TL'][0] == 'on':
+                _is_auto = True
+                line_reference = recv_dict['TL'][1]
+                track_line(line_reference, power)
+
+            # --- obstacle avoid and obstacle follow ---
+            if recv_dict['OA'] == 'on':
+                _is_auto = True
+                obstacle_avoid(power)
+            elif recv_dict['OF'] == 'off':
+                _is_auto = True
+                obstacle_follow(power)
+
+            # --- move ---
+            if _is_auto and recv_dict['RC'] not in ['rest', 'off', 'stop']:
+                remote_control(mycar, recv_dict['RC'], power)
+            else:
+                remote_control(mycar, recv_dict['RC'], power)
+
+            # ------------------- delay -------------------
+            time.sleep(0.01)
+
+    finally:
+        print("Finished")
+        mycar.stop()
+
+
+if __name__ == "__main__":
+    main()
--- examples/web/start.py
+++ examples/web/start.py
@@ -2,71 +2,26 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
-import socket as Socket
-import os
 from os import system, getlogin
-from picar_4wd import getIP
-# from server.sr import soft_reset
 
 user_name = getlogin()
 
 def start_http_server():
+    print(f'start http server')
     system(f"cd /home/{user_name}/picar-4wd/examples/web/client && sudo python3 -m http.server 80 2>&1 1>/dev/null &")#开启服务器
 
 def close_http_server():
+    print('closing http server')
     system("sudo kill $(ps aux | grep 'http.server' | awk '{ print $2 }') 2>&1 1>/dev/null")
 
 def start_websocket():
-    # print("start_websocket")
+    print(f'start websocket server')
     system(f"cd /home/{user_name}/picar-4wd/examples/web/server && sudo python3 web_server.py 2>&1 1>/dev/null &")
-    # system(f"cd /home/{user_name}/picar_4wd/examples/web/server && sudo python3 web_server.py &")
 
 def close_websocket():
-    # print("close_websocket")
+    print('closing websocket server')
     system("sudo kill $(ps aux | grep 'web_server.py' | awk '{ print $2 }') 2>&1 1>/dev/null")
 
-class restartServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # print("do_get successed")
-        if self.path == '/restart':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            # self.send_header("Content-Length", str(len(self.Page)))
-            self.end_headers()
-            close_websocket()
-            start_websocket()
-            # print("Restart websocket")
-            self.wfile.write("OK".encode())
-        else:
-            print('error', self.path)
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html'.encode())
-            self.end_headers()
-            self.wfile.write('<html><head></head><body>'.encode())
-            self.wfile.write(
-                '<h1>{0!s} not found</h1>'.format(self.path).encode())
-            self.wfile.write('</body></html>'.encode())
-
-if __name__ == '__main__':
-    try:
-        # soft_reset()
-        for _ in range(10):
-            ip = getIP()
-            if ip:
-                break
-            time.sleep(1)
-        port = 9000
-        start_http_server()
-        start_websocket()
-        print("Web example starts at %s" % (ip)) 
-        print("Open http://%s in your web browser to control the car!" % (ip)) 
-        server = HTTPServer((ip, port), restartServer)
-        server.serve_forever()
-      
-    except KeyboardInterrupt:
-        print('KeyboardInterrupt')
-    finally:
-        print("Finished")
-        server.socket.close()
-        close_http_server()
-        close_websocket()
+if __name__ == "__main__":
+    start_http_server()
+    
